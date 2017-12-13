2f9cf03ced81b0d4fabaf0b2c99d131f230427ea
Paulo Henrique Sacramento <contato@henriquesacramento.net>
2017-12-12 22:59:03 -0300

    Correct API URL (AGAIN)

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 export var Constants = {
-  // SERVER_URL: 'https://e-votacao-api.henriquesacramento.net',
-  SERVER_URL: 'http://localhost:5000',
+  SERVER_URL: 'https://e-votacao-api.henriquesacramento.net',
+  // SERVER_URL: 'http://localhost:5000',
   API_URL: 'api/v1'
 }
 
