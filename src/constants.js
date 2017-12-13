1c2ee25599ecaa73bebbdd09fb8397d4f2273581
Paulo Henrique Sacramento <contato@henriquesacramento.net>
2017-12-12 22:08:17 -0300

    Added Secure API url

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 export var Constants = {
   // SERVER_URL: 'https://mcr-reembolso.herokuapp.com',
-  SERVER_URL: 'http://e-votacao-api.henriquesacramento.net',
+  SERVER_URL: 'https://e-votacao-api.henriquesacramento.net',
   API_URL: 'api/v1'
 }
 
